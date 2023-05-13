0eed37050190a5a59a382f96550c59612f539982
Sergey Piunov <piunov92@gmail.com>
2023-05-14 00:58:56 +0300

    added class Car

---
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -0,0 +1,38 @@
+package hiber.model;
+
+import javax.persistence.*;
+import javax.persistence.criteria.CriteriaBuilder;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "series")
+    private Integer series;
+
+    @Column(name = "model")
+    private String model;
+
+    public Car() {
+    }
+
+    public Integer getSeries() {
+        return series;
+    }
+
+    public void setSeries(Integer series) {
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
